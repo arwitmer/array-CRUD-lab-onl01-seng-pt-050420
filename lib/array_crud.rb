7439a8084bf769d34b6941f30c90a77ccc74deeb
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 06:01:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["I", "am", "learning"]
-  retrieve_element_from_index
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
