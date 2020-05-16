f84909bc73babfbf493da5cca00ca439b32faaac
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 05:37:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   ["Elkhart", "Memphis", "Lancaster"]
-  
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
