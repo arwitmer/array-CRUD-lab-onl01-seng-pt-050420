46286ebbbac8228649540532bf466f1b922ea452
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 05:54:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["andy", "aaron"]
+  add_element_to_end_of_array = ["andy", "aaron"]
   add_element_to_end_of_array << "arrays!"
 end
 
